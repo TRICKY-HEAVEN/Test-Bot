2d4785b054fa09e01e6227dc11ecefc03aa16b8e
noob-bholu <143063747+Noob-Bholu@users.noreply.github.com>
2023-09-25 07:49:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
 
+import os 
+ API_ID = int(os.environ.get("API_ID", None)) 
+ API_HASH = os.environ.get("API_HASH", None)  
+ BOT_TOKEN = os.environ.get("BOT_TOKEN", None)  
+ OPENAI_KEY = os.environ.get("OPENAI_KEY",None) 
+ BOT_USERNAME = os.environ.get("BOT_USERNAME" ,None)  
+ SUDO=os.environ.get("SUDO",None) 
+ BOT_NAME = os.environ.get("BOT_NAME",None) 
+ START_IMG = os.environ.get("START_IMG","https://telegra.ph/file/00ab5dbe87cd68ab0ef83.jpg") 
+ STKR = os.environ.get("STKR","CAACAgUAAx0CcFjuBQACspVk9vwdxiLoNUbxu1jaT3Cim3J_8AACgwkAAiIraFfTpm9JLn9D9TAE-FvC2SBJnd-vAAAokCAALW9iBFzemsQBDIqWkuBA") 
+ BANNED_USERS=os.environ.get("BANNED_USERS",None) 
+ UPDATE_CHNL = os.environ.get("UPDATE_CHNL",None) 
+ OWNER_USERNAME = os.environ.get("OWNER_USERNAME",None) 
+ SUDO=os.environ.get("SUDO",None) 
+ SUPPORT_GRP = os.environ.get("SUPPORT_GRP","") 
+ 
